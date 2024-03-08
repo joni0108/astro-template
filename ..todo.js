cd7c31e971750562258dd3c2b06c00bca9d4ecf8
joni0108 <trollpro0108@icloud.com>
2024-03-08 11:02:49 -0800

    Added initials

---
+++ ..todo.js
@@ -0,0 +1,8 @@
+//List of actions to be completed once the repository has been cloned
+//TODO: Run npm install to install all the modules
+//TODO: Go to src/components/SEO.astro and update all the values to your project
+//TODO: Add the [SEO] og image to the public folder
+//TODO: Add the verification on the Google Search Console
+//TODO: Update the packages.json file with the correect name
+//TODO: Update the src/components/typography files to your desired font sizes, styles, and colors.
+//TODO: Update the tailwind.config.mjs file with your color pallete and fonts (and any other config that applies)
